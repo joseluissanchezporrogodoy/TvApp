01b41ed1bd489ab26a4afc7fa9c4532eb3a206a3
José Luis Sanchez Porro <jose.sanchezporro@mirada.tv>
2018-01-11 08:52:48 +0100

    Initial Commit

---
+++ FocusSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FocusSample
+//
+//  Created by José Luis Sanchez Porro on 11/1/18.
+//  Copyright © 2018 José Luis Sanchez Porro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
